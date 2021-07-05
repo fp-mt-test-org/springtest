93d8914763f99c8e0347c596256388d73896b848
Matthew Thomas <matthewthomas@flexport.com>
2021-07-04 18:48:50 -0700

    Fix deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -17,4 +17,4 @@ set -e
 
 ./scripts/setup-kubectl-for-gcloud.sh
 
-helm upgrade --install --atomic springtest src/main/helm -n default -f src/main/helm/values-$ENV.yaml --set config.image.tag=$TAG
+helm upgrade --install --atomic springtest src/main/helm -n default -f src/main/helm/values-"${ENV}.yaml" --set config.image.tag="${TAG}"
